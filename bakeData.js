d8c890f1d3c290efa6dca4f24d829688625984ae
hoyis47 <101969111+hoyis47@users.noreply.github.com>
2026-06-09 16:16:25 +0900

    Add files via upload

--- bakeData.js
+++ bakeData.js
@@ -25,5 +25,31 @@ const bakeryListData = [
       "time": { "min": 18, "max": 20 }
     },
     "memo": "성형:눌러펴기 -> 말기 -> 35~40cm 길이로 늘리기"
+  },
+  {
+    "name": "베이글",
+    "mixing": "발전단계",
+    "firstFermentation": { "time": { "min": 45, "max": 45 } },
+    "intermediateProof": { "time": { "min": 10, "max": 15 } },
+    "secondFermentation": { "time": { "min": 20, "max": 20 } },
+    "bake": {
+      "topHeat": 210,
+      "bottomHeat": 190,
+      "time": { "min": 18, "max": 20 }
+    },
+    "memo": "성형:눌러펴기 -> 말기 -> 27~30cm 길이로 늘리기 -> 링모양 만들어 꼬집기 <br>80도 끓는물에 5초씩 양면데치기"
+  },
+  {
+    "name": "호밀빵",
+    "mixing": "발전단계",
+    "firstFermentation": { "time": { "min": 40, "max": 40 } },
+    "intermediateProof": { "time": { "min": 10, "max": 10 } },
+    "secondFermentation": { "time": { "min": 27, "max": 30 } },
+    "bake": {
+      "topHeat": 220,
+      "bottomHeat": 200,
+      "time": { "min": 28, "max": 30 }
+    },
+    "memo": "성형:눌러펴기 -> 럭비공 모양으로 말기 -> 봉하기 -> 22~23센티로 살짝 밀기"
   }
 ];
